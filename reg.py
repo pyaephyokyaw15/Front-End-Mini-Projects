9b0dc9c1213cb39f645e2e3a40be1f8f4afd9ee2
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-07-03 20:43:18 +0630

    Delete unnecessary files

--- reg.py
+++
@@ -1,6 +0,0 @@
-import re
-
-video_url = 'https://www.tiktok.com/@minie13ever13/video/7053416636539358465'
-tkRegex = r'.*tiktok.com*/.*/.*/(.*)'
-tkMatch = re.search(tkRegex, video_url)
-print(tkMatch.groups()[0])
